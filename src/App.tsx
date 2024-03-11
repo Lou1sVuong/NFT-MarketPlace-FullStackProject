783f9494fd59ac32f3e36bf43751ddba85db6981
Mai V. Xuan Vuong <xuannvuongg@gmail.com>
2024-03-12 02:35:10 +0700

    update icons and images in assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-
+import monkeyDance from './assets/images/avatars/monkeyD.svg'
+import icon from './assets/images/infoCard/CreateCollectionIcon.svg'
 function App() {
   return (
     <>
-    
+    <div className="bg-gradient2 p-8">
+      <h1 className="textW">Hello, Tailwind CSS Color Test!</h1>
+      <button className="bg-callAction textW px-4 py-2 mt-4">Call to Action</button>
+    </div>
+    <div className="bg-gradient1 p-8">
+      <h1 className="textW">Hello, Tailwind CSS Color Test!</h1>
+      <button className="bg-callAction textW px-4 py-2 mt-4">Call to Action</button>
+    </div>
+    <img src={monkeyDance} alt="" />
+    <img src={icon} alt="" />
+
+
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      
+    extend: {
+      colors: {
+        // Background & Text
+        'bg': '#3B3B3B',
+        'bgsecondary': '#3B3B3B',
+        'captiontext': '#3B3B3B',
+        'textW': '#FFF',
+        // Call to Action
+        'callAcction': '#A259FF',
+      },
+      backgroundImage: {
+        // Gradients
+        'gradient1': 'linear-gradient(101deg, #A259FF 13.57%, #FF6250 97.65%)',
+        'gradient2': 'linear-gradient(128deg, #A259FF 49.75%, #377DF7 136.56%)',
+
+    }
     },
-    extend: {},
   },
   plugins: [],
 }
